4e8c80f9db425330d934257aac8a4defb298b312
ABuffSeagull <reecevanatta@gmail.com>
2019-12-03 23:02:20 -0500

    Sorta working holds
    Breaks on the stutter step though

--- convert.py
+++ convert.py
@@ -128,7 +128,7 @@ if len(stops) != 0:
         stops_map[float(start_str)] = float(value_str)
 
 
-def get_stop_offset(current_beat: int) -> float:
+def get_stop_offset(current_beat: float) -> float:
     current_bpm = 0.0
     for start, value in bpm_map.items():
         if current_beat >= start:
@@ -137,41 +137,56 @@ def get_stop_offset(current_beat: int) -> float:
             break
     current_offset = 0.0
     for start, value in stops_map.items():
-        if current_beat >= start:
+        if current_beat > start:
             current_offset += value
         else:
             break
-    return beats_to_ticks(current_offset * (current_bpm * 60))
+    return (current_offset / 60) * current_bpm * RESOLUTION
 
+
+difficulty_map = {
+    "Beginner": "Beginner",
+    "Easy": "Easy",
+    "Medium": "Medium",
+    "Hard": "Hard",
+    "Challenge": "Expert",
+}
 
 # NOTES
-note_list: List[Note] = []
-bars_full = (
-    cast(Match[str], re.search(r"#NOTES.*?(\d{4}.*?);", stepfile, re.DOTALL))
-    .group(1)
-    .split(",")
-)
-bars = [single_bar.strip().split() for single_bar in bars_full]
+for note_match in re.finditer(
+    r"#NOTES:\s+dance-single:.*?:\s+(?P<difficulty>\w+):.*?(?P<notes>\d{4}\s.*?;)",
+    stepfile,
+    re.DOTALL,
+):
+    note_list: List[Note] = []
+    bars_full = note_match.group("notes").split(",")
+    bars = [single_bar.strip().split() for single_bar in bars_full]
 
-print("[ExpertSingle]\n{\n\t0 = E PART GUITAR\n\t0 = E play", file=OUTPUT_FILE)
-for bar_index, single_bar in enumerate(bars):
-    metric = 4 / len(single_bar)
-    for beat_index, beat in enumerate(single_bar):
-        if beat.count("0") == 4:
-            continue
-        ticks = beats_to_ticks(bar_index * 4 + beat_index * metric)
-        for dir_index, isHit in enumerate(beat):
-            if isHit == "1" or isHit == "2" or isHit == "4":
-                note_list.append(
-                    Note(RESOLUTION, bar_index * 4 + beat_index * metric, dir_index)
-                )
-            elif isHit == "3":
-                note = reverse_find_direction(note_list, dir_index)
-                note.set_duration(RESOLUTION, bar_index * 4 + beat_index * metric)
+    difficulty = difficulty_map[note_match.group("difficulty")]
 
-for note in filter(lambda note: not blacklister.is_blacklisted(note.beat), note_list):
-    tick = round(note.tick + tick_offset)
-    print(f"\t{tick} = N {note.direction} {note.duration}", file=OUTPUT_FILE)
-    print(f"\t{tick} = N 6 0", file=OUTPUT_FILE)
+    print(
+        f"[{difficulty}Single]\n{{\n\t0 = E PART GUITAR\n\t0 = E play", file=OUTPUT_FILE
+    )
+    for bar_index, single_bar in enumerate(bars):
+        metric = 4 / len(single_bar)
+        for beat_index, beat in enumerate(single_bar):
+            if beat.count("0") == 4:
+                continue
+            ticks = beats_to_ticks(bar_index * 4 + beat_index * metric)
+            for dir_index, isHit in enumerate(beat):
+                if isHit == "1" or isHit == "2" or isHit == "4":
+                    note_list.append(
+                        Note(RESOLUTION, bar_index * 4 + beat_index * metric, dir_index)
+                    )
+                elif isHit == "3":
+                    note = reverse_find_direction(note_list, dir_index)
+                    note.set_duration(RESOLUTION, bar_index * 4 + beat_index * metric)
 
-print("}", file=OUTPUT_FILE)
+    for note in filter(
+        lambda note: not blacklister.is_blacklisted(note.beat), note_list
+    ):
+        tick = round(note.tick + tick_offset + get_stop_offset(note.beat))
+        print(f"\t{tick} = N {note.direction} {note.duration}", file=OUTPUT_FILE)
+        print(f"\t{tick} = N 6 0", file=OUTPUT_FILE)
+
+    print("}", file=OUTPUT_FILE)
